e57ce7515203c3fa059e4831a2f35866e3682aab
Quinton <qemiller@vt.edu>
2019-02-05 15:03:55 -0500

    added search functionality to parser

---
+++ src/parse.java
@@ -0,0 +1,84 @@
+/**BST can search, insert, and remove right now.**/ 
+/**Parser for Dictionary ADT**/
+
+import java.io.File;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class parse {
+	public void parseFile(String fileName) {
+		try
+		{
+			Scanner sc = new Scanner(new File(fileName));
+			bst<String, rectangle> tree = new bst<String, rectangle>();
+			while (sc.hasNextLine()) {
+				String command = sc.next();
+				String name;
+				int x;
+				int y;
+				int w;
+				int h;
+				switch (command) {
+				
+				case "insert":
+					name = sc.next();
+					x = sc.nextInt();
+					y = sc.nextInt();
+					w = sc.nextInt();
+					h = sc.nextInt();
+					rectangle rect = new rectangle(name, x, y, w, h);
+					tree.insert(rect.getName(), rect);
+					System.out.println(rect.getName() + " inserted into tree! ");
+					break;
+				
+				case "remove":
+					name = sc.next();
+					if(containsNumber(name))
+					{	
+						x = Integer.parseInt(name);
+						y = sc.nextInt();
+						w = sc.nextInt();
+						h = sc.nextInt();
+					}
+					else
+					{
+						if(tree.remove(name) == null)
+							System.out.println("No rectangle named "+ name +" in tree." );
+						else
+							System.out.println(name + " removed from tree");
+					}
+					break;
+					
+				case "dump":
+					tree.dump();
+					break;
+					
+				case "search":
+					name = sc.next();
+					rectangle searchRect = tree.search(name);
+					if(searchRect!=null)
+					{
+						System.out.println(searchRect.toString());
+					}
+					
+					else
+						System.out.println(name + " could not be found.");
+					break;
+					
+					
+				default:
+					System.out.println("Unrecognized input " + command);
+				}
+			}
+			sc.close();
+		}
+		catch(Exception FileNotFoundException)
+		{}
+	}
+	private boolean containsNumber(String s)
+	{
+		return Pattern.compile("[0-9]").matcher(s).find();
+	}
+}
+
+
---
+++ src/rectangle.java
@@ -0,0 +1,78 @@
+public class rectangle {
+	private String name;
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	private int xEnd;
+	private int yEnd;
+	
+	
+	rectangle(String nm, int xVar, int yVar, int w, int h){
+		name = nm;
+		x = xVar;
+		y = yVar;
+		width = w;
+		height = h;
+		xEnd = xVar + w;
+		yEnd = yVar + h;
+	}
+	
+	public String toString() {
+		return "(" + name + ", " + x + ", " + y + ", " + width + ", " + height + ")";
+	}
+	
+	public String rectString() {
+		return "(" + name + ", " + x + ", " + y + ", " + width + ", " + height + ")";
+	}
+	
+	//Used for regionsearch
+	public boolean isContained(int xVar, int yVar, int w, int h) {
+		if((x >= xVar && x <= (xVar + w)) || (xEnd <= xVar && xEnd >= (xVar + w))) {
+			//Intersection with x
+			if((y >= yVar && y <= (yVar + h)) || (yEnd <= yVar && yEnd >= (yVar + h))) {
+				//Intersection with y
+				return true;
+			}
+			else return false;
+		}
+		else return false;
+	}
+	
+	//Used for remove
+	public boolean isExact(int xVar, int yVar, int w, int h) {
+		if(x != xVar || y != yVar || width != w || height != h) {
+			return false;
+		}
+		else return true;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getXVal() {
+		return x;
+	}
+	
+	public int getYVal() {
+		return y;
+	}
+
+	public int getXEnd() {
+		return xEnd;
+	}
+	
+	public int getYEnd() {
+		return yEnd;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+}
