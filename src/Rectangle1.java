2a0ec635ea777976535f193818b33d95738976ed
Quinton <qemiller@vt.edu>
2019-02-07 21:41:12 -0500

    finished all functions except for intersect, tested and debgged against the given txt files, added comments to explain code.

--- src/Rectangle1.java
+++ src/Rectangle1.java
@@ -1,10 +1,23 @@
+/*Compiler: jdk 1.8.0_201
+ *Operating System: Windows 10
+ *Date Completed:
+ *
+ *This program takes in a set of commands from a txt file that will store and query rectangles.
+ *The data structure used to store the rectangles is a Binary Search Tree. The commands that are
+ *implemented in this program are:
+ *insert, which inserts rectangles into the tree
+ *remove by both coordinate and name, which will remove the given rectangle by name or coordinate
+ *regionsearch, which will output any rectangle in the given region
+ *search by name, which will search the tree for rectngle with the given name
+ *dump, which will dump the rectangles in the binary tree to the console
+ **/
 
 public class Rectangle1{
 	public static void main(String[] args)
 	{
 		String fileName = args[0];
-		parse temp = new parse();
-		temp.parseFile(fileName);
+		parse Rectangle1 = new parse();
+		Rectangle1.parseFile(fileName);
 	}
 
 }
--- src/parse.java
+++ src/parse.java
@@ -1,97 +1,161 @@
-/**BST can search, insert, and remove right now.**/ 
+/**BST can search, insert, and remove right now.**/
 /**Parser for Dictionary ADT**/
 
 import java.io.File;
 import java.util.Scanner;
-import java.util.regex.Pattern;
+import java.lang.Character;
 
+/*
+ This function parses the file and executes the given commands
+ @param fileName is name of file to be parsed
+ */
 public class parse {
 	public void parseFile(String fileName) {
-		try
-		{
+		try {
 			Scanner sc = new Scanner(new File(fileName));
 			bst<String, rectangle> tree = new bst<String, rectangle>();
 			while (sc.hasNextLine()) {
-				String command = sc.next();
-				String name;
-				int x;
-				int y;
-				int w;
-				int h;
+				String command = sc.next();//command read in from file
+				String name;//name of rectangle
+				int x;//x-coordinate of rectangle
+				int y;//y-coordinate of rectangle
+				int w;//width of rectangle
+				int h;//height of rectangle
 				switch (command) {
-				
-				case "insert":
+
+				case "insert"://deal with the insert command
 					name = sc.next();
 					x = sc.nextInt();
 					y = sc.nextInt();
 					w = sc.nextInt();
 					h = sc.nextInt();
-					rectangle rect = new rectangle(name, x, y, w, h);
-					tree.insert(rect.getName(), rect);
-					System.out.println(rect.getName() + " inserted into tree! ");
+					rectangle rectInsert = new rectangle(name, x, y, w, h);//rectangle to be inserted
+					if (Character.isLetter(name.charAt(0))) {
+						if (coordinatesCorrect(x, y, w, h)) {
+							tree.insert(rectInsert.getName(), rectInsert);
+							System.out.println("Rectangle accepted: " + rectInsert.toString());
+						} else
+							System.out.println("Rectangle rejected: " + rectInsert.toString());
+					} else
+						System.out.println("Rectangle rejected: " + rectInsert.toString());
 					break;
-				
-				case "remove":
+
+				case "remove"://deal with the remove command
 					name = sc.next();
-					if(containsNumber(name))
-					{	
+					if (Character.isDigit(name.charAt(0))) { 
 						x = Integer.parseInt(name);
 						y = sc.nextInt();
 						w = sc.nextInt();
 						h = sc.nextInt();
-						removeCoordinate(x,y,w,h,tree);
-						System.out.println("rectangle at " + x + ", " + y + ", " + w + ", "+ h + " removed!");
-					}
-					else
-					{
-						if(tree.remove(name) == null)
-							System.out.println("No rectangle named "+ name +" in tree." );
-						else
-							System.out.println(name + " removed from tree");
+						if (removeCoordinate(x, y, w, h, tree)) {
+						} else
+							System.out.println("Rectangle rejected (" + x + "," + y + "," + w + "," + h + ")");
+					} else {
+						if (tree.remove(name) == null)
+							System.out.println("Rectangle rejected " + name);
 					}
 					break;
-					
-				case "dump":
+
+				case "dump"://deal with the dump command
 					tree.dump();
 					break;
-					
-				case "search":
+
+				case "search"://deal with the search command
 					name = sc.next();
-					rectangle searchRect = tree.search(name);
-					if(searchRect!=null)
-					{
-						System.out.println(searchRect.toString());
+					boolean foundOne = false;//Have I found one rectangle with the given name
+					bstNode<String, rectangle> currNode = tree.getFirst();//current node the search is on
+					while (currNode != null) {
+						if (currNode.element().getName() == name) {
+							System.out.println("Rectangle Found:" + currNode.element().toString());
+							currNode = tree.getNext(currNode);
+							foundOne = true;//I have found a rectangle while I was searching
+						} else
+							currNode = tree.getNext(currNode);
 					}
-					
-					else
-						System.out.println(name + " could not be found.");
+					if (!foundOne)//If I haven't found a rectangle while I was searching, throw an error
+						System.out.println("Rectangle not found:" + name);
 					break;
-	
+
+				case "regionsearch"://deal with the regionsearch command
+					x = sc.nextInt();
+					y = sc.nextInt();
+					w = sc.nextInt();
+					h = sc.nextInt();
+					bstNode<String, rectangle> currNodeRegion = tree.getFirst();//current node for regionsearch
+					System.out.println("Rectanagles intersecting region  (" + x + "," + +y + "," + w + "," + h + "):");
+					while (currNodeRegion != null) {
+						if (currNodeRegion.element().isContained(x, y, w, h)) {
+							System.out.println(currNodeRegion.element().toString() + "\n");
+							currNodeRegion = tree.getNext(currNodeRegion);
+						} else
+							currNodeRegion = tree.getNext(currNodeRegion);
+					}
+					break;
+
+				case "intersections"://deal with the intersections command
+					System.out.println("Intersection pairs:");
+					break;
+					
 				default:
 					System.out.println("Unrecognized input " + command);
 				}
 			}
 			sc.close();
+		} catch (Exception FileNotFoundException) {//Throw this if I can't find the file
 		}
-		catch(Exception FileNotFoundException)
-		{}
 	}
-	private boolean containsNumber(String s)
-	{
-		return Pattern.compile("[0-9]").matcher(s).find();
-	}
-	
-	private void removeCoordinate(int x, int y, int w, int h, bst<String,rectangle> tree)
-	{
+
+	/*
+	 * will remove a rectangle by coordinate from the tree
+	 * 
+	 * @param x is x coordinate of rectangle
+	 * 
+	 * @param y is y coordinate of rectangle
+	 * 
+	 * @param w is width of rectangle
+	 * 
+	 * @param h is height of rectangle
+	 * 
+	 * @param is the tree to search for the given rectangle
+	 * 
+	 * @return if there was something removed from the tree
+	 */
+	private boolean removeCoordinate(int x, int y, int w, int h, bst<String, rectangle> tree) {
+		boolean removedOne = false;
 		bstNode<String, rectangle> curNode = tree.getFirst();
-		while(curNode != null)
-		{
-			if(curNode.element().isExact(x, y, w, h))
+		while (curNode != null) {
+			if (curNode.element().isExact(x, y, w, h)) {
 				tree.remove(curNode.element().getName());
-			else
-				curNode=tree.getNext(curNode);
+				removedOne = true;
+				break;
+			} else
+				curNode = tree.getNext(curNode);
 		}
+		if (!removedOne)
+			return false;
+		else
+			return true;
 	}
+
+	/*
+	 * Checks to make sure the given coordinates are within the world box and width
+	 * and height are greater than zero.
+	 * 
+	 * @param x is x coordinate of rectangle
+	 * 
+	 * @param y is y coordinate of rectangle
+	 * 
+	 * @param w is width of rectangle
+	 * 
+	 * @param h is height of rectangle
+	 * 
+	 * @return if the coordinates are correct
+	 */
+	private boolean coordinatesCorrect(int x, int y, int w, int h) {
+		if (x + w <= 1024 && x >= 0 && y + h <= 1024 && y >= 0 && w > 0 && h > 0)
+			return true;
+		else
+			return false;
+	}
+
 }
-
-
